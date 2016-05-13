fc34612773dcbadab420631d497af2404b0fe6c0
Nick Balakhtar <midnightcoldlove@gmail.com>
2016-05-13 19:08:52 +0300

    third part

--- books/models.py
+++ books/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
 	title = models.CharField(max_length=150)
-	author = models.CharField(max_length=150)
+	author = models.CharField(max_length=150, help_text="Hey guy, c'mon your name")
 	review = models.TextField(blank=True,null=True)
 	date_review = models.DateTimeField(blank=True,null=True)
-	is_favourite = models.BooleanField(default=False)
+	is_favourite = models.BooleanField(default=False,verbose_name="Favourite?")
+
+	def __str__(self):
+		return self.title
