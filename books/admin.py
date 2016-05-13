b56dfa25d166627550d71bac5845104cc7fee86a
Nick Balakhtar <midnightcoldlove@gmail.com>
2016-05-13 23:58:31 +0300

    day one part 2

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
+@admin.register(Book)
 # Register your models here.
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	fieldsets= [
+		("Book details",{"fields":["title","authors"]}),
+		("Review",{"fields":["is_favourite","review","date_review"]})
+	]
+
+	readonly_fields = ('date_review',)
+
+	def book_authors(self, obj):
+		return obj.list_authors()
+
+	list_display = ("title","book_authors","date_review","is_favourite")
+
+	book_authors.short_description = "Author(s)"
+	list_editable = ("is_favourite",)
+	list_display_links = ("title","date_review",)
+	list_filter = ("is_favourite",)
+	search_fields = ("title","authors__name",)
+
+
+admin.site.register(Author)
--- books/models.py
+++ books/models.py
@@ -1,12 +1,28 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Book(models.Model):
 	title = models.CharField(max_length=150)
-	author = models.CharField(max_length=150, help_text="Hey guy, c'mon your name")
+	authors = models.ManyToManyField("Author",related_name="books")
 	review = models.TextField(blank=True,null=True)
 	date_review = models.DateTimeField(blank=True,null=True)
 	is_favourite = models.BooleanField(default=False,verbose_name="Favourite?")
 
 	def __str__(self):
-		return self.title
+		return "{} by {}".format(self.title, self.list_authors()) 
+
+	def list_authors(self):
+		return ", ".join([author.name for author in self.authors.all() ])
+
+	def save(self, *args, **kwargs):
+		if(self.review and self.date_review is None):
+			self.date_review = now()
+
+		super(Book, self).save(*args,**kwargs)
+
+class Author(models.Model):
+	name = models.CharField(max_length=150,help_text="Hey guy, c'mon your name",unique=True)
+
+	def __str__(self):
+		return self.name
